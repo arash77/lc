7d9327e8bb8cb36b4558fc3d02c745ea1fce582e
Arash <kad.arash@email.com>
2021-05-07 02:17:10 +0430

    v0.3

--- sel.py
+++ sel.py
@@ -41,7 +41,8 @@ while True:
         d.find_element_by_id("email").send_keys("arash77.kad@gmail.com")
         d.find_element_by_id("password").send_keys("arash1377")
         d.find_element_by_xpath("//button[@type='submit']").click()
-        time.sleep(10)
+        WebDriverWait(d, 30).until(EC.presence_of_element_located((By.CLASS_NAME, 'MuiAvatar-root')))
+        time.sleep(5)
         flag = True
     else:
         theList = WebDriverWait(d, 300).until(EC.presence_of_element_located((By.CLASS_NAME, 'MuiTableBody-root'))).text.splitlines()
@@ -51,26 +52,23 @@ while True:
         data_of_web = list()
         list_of_coins = [theList[n:n+7] for n in range(0, len(theList), 7)]
         print(list_of_coins[-1], time.ctime())
-        for i in list_of_coins:
-            try:
+        try:
+            for i in list_of_coins:
                 if 'N/A' in i[6].split()[9]:
                     z = i[6].split()
-                    z[9] = "Na Na"
+                    z[9] = "N/A N/A"
                     i[6] = " ".join(z)
                 if int(i[6].split()[17].replace(',', '')) > 1 and int(i[6].split()[16].replace(',', '')) > 1:
-                    data_of_web.append(
-                        dict(
-                            coin = i[2],
-                            galaxy_score = float(i[4]),
-                            alt_rank = int(i[5].split()[0].replace(',', '')),
-                            social_score = int(i[6].split()[12].replace(',', '')),
-                            social_contributors = int(i[6].split()[13].replace(',', '')),
-                            average_sentiment = float(i[6].split()[15].split("%")[0].replace(',', '')),
-                            BullBear = int(i[6].split()[16].replace(',', ''))/int(i[6].split()[17].replace(',', '')),
-                        )
-                    )
-            except Exception as e:
-                print(e, time.ctime())
+                    data_of_web.append(dict(
+                        coin = i[2],
+                        galaxy_score = float(i[4]),
+                        alt_rank = int(i[5].split()[0].replace(',', '')),
+                        social_score = int(i[6].split()[12].replace(',', '')),
+                        social_contributors = int(i[6].split()[13].replace(',', '')),
+                        average_sentiment = float(i[6].split()[15].split("%")[0].replace(',', '')),
+                        BullBear = int(i[6].split()[16].replace(',', ''))/int(i[6].split()[17].replace(',', '')),))
+        except Exception as e:
+            print(e, time.ctime())
         df_new = pd.DataFrame(data_of_web)
         if 'data.csv' in os.listdir(os.getcwd()):
             df_old = pd.read_csv('data.csv')
@@ -86,22 +84,18 @@ while True:
                 Markets[exchange] = [market['base'] for market in getattr(ccxt, exchange)({'timeout': 5000,'enableRateLimit': True}).fetchMarkets() if market['quote'] == "USDT"]
             except Exception as e:
                 print(e, time.ctime())
-        df_new = df_new.assign(exchange=None)
-        for i in range(len(df_new)):
-            df_new.loc[i, 'exchange'] = ",".join([exch for exch, j in Markets.items() if df_new.loc[i, 'coin'] in j])
+
+        df_new['exchange'] = df_new['coin'].apply(lambda row: ",".join([exch for exch, j in Markets.items() if row in j]))
         df_new['exchange'].replace('', np.nan, inplace=True)
         df_new = df_new.dropna(subset=['exchange'])
 
         df = df_new.reset_index(drop=True)
-        df = df.assign(social_score_change=0)
         if 'social_score_old' in df.columns:
-            for i in range(len(df)):
-                if df.loc[i, 'social_score_old']!=0:
-                    df.loc[i, 'social_score_change'] = (df.loc[i, 'social_score'] - df.loc[i, 'social_score_old']) 
-                    try:
-                        df.loc[i, 'social_score_change_percent'] = ((df.loc[i, 'social_score_change'] * 100 / df.loc[i, 'social_score_old']) + df.loc[i, 'social_score_change_percent']) / 2
-                    except:
-                        df.loc[i, 'social_score_change_percent'] = (df.loc[i, 'social_score_change'] * 100 / df.loc[i, 'social_score_old'])
+            df['social_score_change'] = df['social_score'] - df['social_score_old']
+            notnulls = df['social_score_change_percent'].notnull()
+            df.loc[notnulls, 'social_score_change_percent'] = ((df.loc[notnulls, 'social_score_change'] * 100 / df.loc[notnulls, 'social_score_old']) + df.loc[notnulls, 'social_score_change_percent']) / 2
+            isnulls = df['social_score_change_percent'].isnull()
+            df.loc[isnulls, 'social_score_change_percent'] = (df.loc[isnulls, 'social_score_change'] * 100 / df.loc[isnulls, 'social_score_old'])
         df = df.assign(rate=0)
         for rank, asc in [["galaxy_score",False],["BullBear",False]]:
             df = df.sort_values(rank,ascending=asc,ignore_index=True)
@@ -111,8 +105,7 @@ while True:
         df.insert(0, 'ID', range(1, 1+len(df)))
         if 'ID_old' in df.columns:
             df.rename({'ID_change': 'ID_2change'}, axis='columns')
-            for i in range(len(df)):
-                df.loc[i, 'ID_change'] = (df.loc[i, 'ID_old'] - df.loc[i, 'ID'])
+            df['ID_change'] = df['ID_old'] - df['ID']
         
         print(df.head(30), time.ctime())
         df.to_csv('data.csv',index=False)
